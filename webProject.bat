8c8176b7868453149c0358439ee81f0aac909a1e
billy93 <billyfebram@gmail.com>
2021-12-02 22:38:25 +0700

    Test multiplayer

---
+++ webProject.bat
@@ -0,0 +1,1 @@
+browser-sync start --server --files "js/*.js,html/*.html,js/plugins/*.js,js/plugins/Alpha_NETZ_Libs/*.js,js/plugins/Alpha_NETZ_Libs/5_MZ_Scenes/*.js,js/plugins/Alpha_NETZ_Libs/Alpha_NETZ_H/*.js,js/plugins/Alpha_NETZ_Libs/_CFCompiled/*.js,*.*"
